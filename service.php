cb9045ffba014006f83c49ea039390bc6ef7cdcb
Franck Roth <franck7980@gmail.com>
2022-03-28 19:18:02 +0200

    mise en plce Grid et ajout page Team

--- service.php
+++ service.php
@@ -2,9 +2,9 @@
 	
 	function getDatabaseConnexion() {
 		try {
-		    $user = "root";
+		    $user = "";
 			$pass = "";
-			$pdo = new PDO('mysql:host=localhost;dbname=tuto_php', $user, $pass);
+			$pdo = new PDO('mysql:host=localhost;dbname=Nouvelle_Licorne', $user, $pass);
 			 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			return $pdo;
 			
---
+++ tableauEvents.php
@@ -0,0 +1,50 @@
+<table class="table table-striped table-hover">
+  <thead>
+    <tr>
+      <th scope="col">Evenements</th>
+      <th scope="col">Titre</th>
+      <th scope="col">Lieu</th>
+      <th scope="col">Date</th>
+      <th scope="col">Catégories</th>
+      <th scope="col">Matériel</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Lorem Elsass ipsum und DNA</th>
+      <td>Lorem Elsass ipsum und DNA</td>
+      <td>Lorem Elsass ipsum und DNA</td>
+      <td>Lorem Elsass ipsum und DNA</td>
+      <td>Lorem Elsass ipsum und DNA</td>
+      <td>Lorem Elsass ipsum und DNA</td>
+    </tr>
+
+    <tr>
+      <th scope="row">Lorem Elsass ipsum und DNA</th>
+      <td>Lorem Elsass ipsum und DNA</td>
+      <td>Lorem Elsass ipsum und DNA</td>
+      <td>Lorem Elsass ipsum und DNA</td>
+      <td>Lorem Elsass ipsum und DNA</td>
+      <td>Lorem Elsass ipsum und DNA</td>
+    </tr>
+
+    <tr>
+      <th scope="row">Lorem Elsass ipsum und DNA</th>
+      <td>Lorem Elsass ipsum und DNA</td>
+      <td>Lorem Elsass ipsum und DNA</td>
+      <td>Lorem Elsass ipsum und DNA</td>
+      <td>Lorem Elsass ipsum und DNA</td>
+      <td>Lorem Elsass ipsum und DNA</td>
+    </tr>
+
+    <tr>
+      <th scope="row">Lorem Elsass ipsum und DNA</th>
+      <td>Lorem Elsass ipsum und DNA</td>
+      <td>Lorem Elsass ipsum und DNA</td>
+      <td>Lorem Elsass ipsum und DNA</td>
+      <td>Lorem Elsass ipsum und DNA</td>
+      <td>Lorem Elsass ipsum und DNA</td>
+    </tr>
+
+  </tbody>
+</table>
