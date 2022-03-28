42d81a23467096b0afd48558d8d1e617693f3c9f
Franck Roth <franck7980@gmail.com>
2022-03-28 17:36:07 +0200

    add modif de Baris

--- navbar.php
+++ navbar.php
@@ -1,15 +1,26 @@
-  
-<ul class="nav">
-
-  <li class="nav-item">
-    <a class="nav-link" href="index.php">Home</a>
-  </li>
-  
-  <li class="nav-item">
-    <a class="nav-link" href="evenements.php">Evenements</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="bénévoles.php">Bénévoles</a>
-  </li>
-  
-</ul>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<a href ="index.php" ><img class="Logo"
+                    src="./img/Logo.png"
+                    alt="Logo NouvelleLicorne"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./bénévoles.php">Nos bénévoles</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./evenements.php">Nos événements</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
