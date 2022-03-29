ad2f6f8f26fec23a4c7bad59eadd6da862a2fe6a
Franck Roth <franck7980@gmail.com>
2022-03-29 18:39:25 +0200

    nettoyage

--- navbar.php
+++ navbar.php
@@ -1,11 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light opacitynav">
-<a href ="index.php" ><img class="Logo"
-                    src="./img/Logo.png"
-                    alt="Logo NouvelleLicorne"></a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+<a href ="index.php" ><img class="Logo" src="./img/Logo.png" alt="Logo NouvelleLicorne"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent" style="justify-content= space-around !important">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent" style="justify-content: space-around !important">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active hovernav" aria-current="page" href="index.php">Accueil</a>
